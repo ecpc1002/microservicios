e8ff20c71743bcbd66ca3b936c5e03bdd076dc8b
Invitado Waira 2 <waira2@sis.virtual.uniandes.edu.co>
2016-03-31 20:05:09 -0500

    'step1'

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,15 +1,4 @@
-function hitme(color) {
+function hitme(params) {
     console.log("Sending");
-    fetch('/messages/',{
-        headers: {
-          'Accept':'application/json',
-          'Content-Type':'application/json'  
-        },
-        method:'post',
-        body:JSON.stringify({
-            color:color,
-            id: Math.floor(Math.random() * 100)
-        })
-    })
     console.log("Sended");
 }
